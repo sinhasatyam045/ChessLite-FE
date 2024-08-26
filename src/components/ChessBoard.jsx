985db71d50c84c99c2690095602e8001a9d056da
priyam125 <sinha.priyam002@gmail.com>
2024-08-26 23:56:43 +0530

    commit

--- src/components/ChessBoard.jsx
+++ src/components/ChessBoard.jsx
@@ -27,10 +27,63 @@ const ChessBoard = () => {
     return () => socket.off("updated_board", handleMove);
   }, [socket]);
 
+  const isValidPawnMove = (piece, from, to, board) => {
+    const isStartingPosition = from[0] === (piece[0] === "A" ? 1 : 6);
+    const isForwardMovement = to[0] - from[0] === (piece[0] === "A" ? 1 : -1);
+
+    if (isForwardMovement) {
+      // Check for single space move
+      if (Math.abs(to[1] - from[1]) === 0 && Math.abs(to[0] - from[0]) === 1) {
+        return true;
+      } else if (
+        isStartingPosition &&
+        Math.abs(to[0] - from[0]) === 2 &&
+        board[to[0] - 1][to[1]] === ""
+      ) {
+        // Check for two-space move from starting position
+        return true;
+      }
+    }
+
+    return false;
+  };
+
+  const isValidH1Move = (piece, from, to) => {
+    return Math.abs(to[0] - from[0]) <= 2 && Math.abs(to[1] - from[1]) <= 2;
+  };
+
+  const isValidH2Move = (piece, from, to) => {
+    return Math.abs(to[0] - from[0]) === 2 && Math.abs(to[1] - from[1]) === 2;
+  };
+
   const handleSquareClick = (row, col) => {
     if (selectedPiece) {
-      socket.emit("make_move", { selectedPiece, position: [row, col], board });
-      setSelectedPiece(null);
+      const from = selectedPiece;
+      const to = [row, col];
+      const piece = board[from[0]][from[1]];
+
+      if (piece.includes("P")) {
+        if (isValidPawnMove(piece, from, to, board)) {
+          socket.emit("make_move", { selectedPiece, position: to, board });
+          setSelectedPiece(null);
+        } else {
+          alert("Invalid pawn move!");
+        }
+      } else if (piece.includes("H1")) {
+        if (isValidH1Move(piece, from, to)) {
+          socket.emit("make_move", { selectedPiece, position: to, board });
+          setSelectedPiece(null);
+        } else {
+          alert("Invalid H1 move!");
+        }
+      } else if (piece.includes("H2")) {
+        if (isValidH2Move(piece, from, to)) {
+          socket.emit("make_move", { selectedPiece, position: to, board });
+          setSelectedPiece(null);
+        } else {
+          alert("Invalid H2 move!");
+        }
+      }
     } else if (board[row][col] !== "") {
       setSelectedPiece([row, col]);
     }
